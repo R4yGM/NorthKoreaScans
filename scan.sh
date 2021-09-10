e9357bf82f1bb54f1522066db529aecd77784831
R4yan <yessou.rayan@gmail.com>
2021-09-10 15:14:23 +0000

    a

---
+++ scan.sh
@@ -0,0 +1,6 @@
+year=$(date +'%Y')
+month=$(date +'%m')
+day=$(date +'%d')
+mkdir -p $year/$month/$day
+filename=$(date +'%Y-%m-%d_scan.txt')
+nmap -p 1-10000 -sV -O -T4 -v -oN $year/$month/$day/$filename -n 175.45.176.0/22
