71c3141abbf87a1ee0f9bf7a3fd620f448845010
R4yan <yessou.rayan@gmail.com>
2021-09-10 19:52:09 +0200

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,10 @@
+git pull
+year=$(date +'%Y')
+month=$(date +'%m')
+day=$(date +'%d')
+mkdir -p $year/$month/$day
+filename=$(date +'%Y-%m-%d_%H_scan.txt')
+nmap -p 1-10000 -sV -O -T4 -v -oN $year/$month/$day/$filename -n 175.45.176.0/22
+git add .
+git commit -m "scan"
+git push -f
