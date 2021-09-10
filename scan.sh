e84bdb70cb7270ec85af79cbfb46adc45d37d3e6
R4yan <yessou.rayan@gmail.com>
2021-09-10 17:22:23 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,5 +2,5 @@ year=$(date +'%Y')
 month=$(date +'%m')
 day=$(date +'%d')
 mkdir -p $year/$month/$day
-filename=$(date +'%Y-%m-%d_scan.txt')
+filename=$(date +'%Y-%m-%d_%H_scan.txt')
 nmap -p 1-10000 -sV -O -T4 -v -oN $year/$month/$day/$filename -n 175.45.176.0/22
