ba97a2d40caa1feb41a955f042b082ef0ef5b05d
R4yan <yessou.rayan@gmail.com>
2021-09-29 22:55:29 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -2,10 +2,10 @@ git pull
 year=$(date +'%Y')
 month=$(date +'%m')
 day=$(date +'%d')
-mkdir -p $year/$month/$day
-filename=$(date +'%Y-%m-%d_%H_scan_UK.txt')
-nmap -p 1-10000 -sV -O -T4 -v -oN $year/$month/$day/$filename -n 175.45.176.0/22
+mkdir -p /root/scan/$year/$month/$day
+filename=$(date +'%Y-%m-%d_%H_scan_DE.txt')
+nmap -p1-10000 -sV -O -T4 -v -oN /root/scan/$year/$month/$day/$filename -n 175.45.176.0/22 -Pn
+git pull
 git add .
 git commit -m "scan"
 git push -f
-#
